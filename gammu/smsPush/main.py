ace0f0c49ccc9bafd594d4956ed2de7f48c1a1f4
TheZihanGu <i@zihangu.cn>
2021-02-02 21:58:59 +0800

    Upload Gammu Helper.
    Main: gammu/smsPush/main.py
    Auther: TheZihanGu

---
+++ gammu/smsPush/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/2/2 下午9:36
+# @Author  : TheZihanGu
+# @File    : main.py
+
+import os
+import requests
+
+Server_IP = "192.168.3.2" # 服务器IP地址
+Port = "8080" # 程序端口号
+alipay_sms = "，请勿向他人泄漏您的验证码！"
+
+if __name__ == "__main__":
+    numParts = int(os.environ['DECODED_PARTS'])
+    text = ''
+    if numParts == 0:
+        text = os.environ['SMS_1_TEXT']
+    else:
+        text = os.environ['DECODED_0_TEXT']
+    if alipay_sms in text:
+        url = "http://" + Server_IP + ":" + Port + "/smg"
+        data = {"msg":text}
+        res = requests.post(url=url,data=data)
