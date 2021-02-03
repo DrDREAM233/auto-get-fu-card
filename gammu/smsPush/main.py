c1240cac9bdb31c9ae1fe8b9c48b131ad8855b38
TheZihanGu <i@zihangu.cn>
2021-02-03 10:56:43 +0800

    Update main.py

--- gammu/smsPush/main.py
+++ gammu/smsPush/main.py
@@ -7,17 +7,19 @@
 import os
 import requests
 
-Server_IP = "192.168.3.2" # 服务器IP地址
+Server_IP = "192.168.3.1" # 服务器IP地址
 Port = "8080" # 程序端口号
-alipay_sms = "，请勿向他人泄漏您的验证码！"
+alipay_sms = "，请勿向他人泄露您的验证码！唯一热线95188"
 
 if __name__ == "__main__":
     numParts = int(os.environ['DECODED_PARTS'])
     text = ''
     if numParts == 0:
         text = os.environ['SMS_1_TEXT']
+
     else:
         text = os.environ['DECODED_0_TEXT']
+
     if alipay_sms in text:
         url = "http://" + Server_IP + ":" + Port + "/msg"
         data = {"msg":text}
